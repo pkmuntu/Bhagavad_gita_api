c800e86bcee02781030116b3dfadc8b325301c86
“Pradeep” <pradeep.singh@atmax.co>
2022-07-17 18:08:08 +0530

    Verse details response modified

--- src/main/java/api/gita/dto/response/VerseResponseDTO.java
+++ src/main/java/api/gita/dto/response/VerseResponseDTO.java
@@ -12,14 +12,14 @@ public class VerseResponseDTO {
 	private Integer bookId;
 	private Integer chapterIndex;
 	private String verseNumber;
-	private String originalVerse_Hi;
+	private String originalVerse_Sn;
 	private String originalVerse_En;
 	private String verseShort;
 	private String commentary;
 
-	public static VerseResponseDTO of(Integer bookId, Integer chapterIndex, String verseNumber, String originalVerse_Hi,
+	public static VerseResponseDTO of(Integer bookId, Integer chapterIndex, String verseNumber, String originalVerse_sn,
 			String originalVerse_En, String verseShort, String commentary) {
-		return new VerseResponseDTO(bookId, chapterIndex, verseNumber, originalVerse_Hi, originalVerse_En,
+		return new VerseResponseDTO(bookId, chapterIndex, verseNumber, originalVerse_sn, originalVerse_En,
 				verseShort, commentary);
 	}
 
